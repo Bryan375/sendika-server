dbcd0d6b5857ac507cf4a910db10b4202e7fbc5c
tensan8 <tensanf@gmail.com>
2021-10-22 03:09:14 +0700

    GraphConv still can predict, no transfer learning

--- sendika_server/api/models.py
+++ sendika_server/api/models.py
@@ -3,7 +3,7 @@ from django.db import models
 # Create your models here.
 class MLModel(models.Model):
     smile_name = models.CharField(max_length=120)
-    predicted_result = models.FloatField()
+    #predicted_result = models.FloatField()
     
     
     def __str__(self):
