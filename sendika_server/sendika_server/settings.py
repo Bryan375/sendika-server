3a0384b97c07566e4c6ba5df0f379fa9a833a5dd
Bryan Austyn Ichsan <101229576@students.swinburne.edu.my>
2021-10-30 13:00:34 +0700

    using env variable

--- sendika_server/sendika_server/settings.py
+++ sendika_server/sendika_server/settings.py
@@ -12,21 +12,28 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 from pathlib import Path
+import environ
+
+env = environ.Env()
+
+# reading .env file
+environ.Env.read_env()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
+
 
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-)&z3=*#*nh2r))xsum^l9p)_jsnqhzpc9vo!0ql=!o*g@bii%r'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = env('ALLOWED_HOSTS').split(',')
 
 
 # Application definition
